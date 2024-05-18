a429037b1f705503c394671bd28e20c14aad8281
pipe-mv <pipe.m.v@hotmail.com>
2024-05-19 00:52:35 +1000

    Coonect local to remote repository

--- index.js
+++ index.js
@@ -2,89 +2,78 @@
 
 let numOfButtons = document.querySelectorAll(".drum").length;
 
-for (let i = 0; i < numOfButtons; i++ ){
-
-
-  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-
+for (let i = 0; i < numOfButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     let buttonInnerHTMl = this.innerHTML;
 
     makeSound(buttonInnerHTMl);
 
     buttonAnimation(buttonInnerHTMl);
-
-    });
-  }
-
+  });
+}
 
 //Detecting Keyboard Press
 
-  document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
 
-        makeSound(event.key);
+  buttonAnimation(event.key);
+});
 
-        buttonAnimation(event.key);
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play(this.innerHTML);
+      break;
 
-  });
-
-
-    function makeSound (key){
-
-    switch (key) {
-      case "w":
-        let tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play(this.innerHTML);
-        break;
-
-      case "a":
-      let tom2 = new Audio('sounds/tom-2.mp3');
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play(this.innerHTML);
       break;
 
-      case "s":
-        let tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play(this.innerHTML);
-        break;
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play(this.innerHTML);
+      break;
 
-      case "d":
-        let tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play(this.innerHTML);
-        break;
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play(this.innerHTML);
+      break;
 
-      case "j":
-        let snare = new Audio('sounds/snare.mp3');
-        snare.play(this.innerHTML);
-        break;
+    case "j":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play(this.innerHTML);
+      break;
 
-      case "k":
-        let crash = new Audio('sounds/crash.mp3');
-        crash.play(this.innerHTML);
-        break;
+    case "k":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play(this.innerHTML);
+      break;
 
-      case "l":
-        let kick = new Audio('sounds/kick-bass.mp3');
-        kick.play(this.innerHTML);
-        break;
+    case "l":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play(this.innerHTML);
+      break;
 
+    default:
+      console.log(buttonInnerHTMl);
+  }
+}
+// this.style.color = "white" ;
+//
 
-      default: console.log(buttonInnerHTMl)
-    }
-    }
-    // this.style.color = "white" ;
-    //
+//What to do when click detected!
 
+// Animation button
 
-    //What to do when click detected!
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
 
-    // Animation button
+  activeButton.classList.add("pressed");
 
-    function buttonAnimation (currentKey){
-
-      let activeButton = document.querySelector ("." + currentKey);
-
-      activeButton.classList.add("pressed");
-
-      setTimeout(function (){
-        activeButton.classList.remove("pressed");
-      }, 100);
-    }
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
